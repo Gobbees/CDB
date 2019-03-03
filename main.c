d47b935720b07d359b632d08c16bc34ed0848e76
Giovanni Gobbi <22635636+Gobbees@users.noreply.github.com>
2019-03-03 09:59:41 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,16 +1,8 @@
-//
-//  main.c
-//  Prog_Algoritmi
-//
-//  Created by Giovanni Gobbi on 27/06/2018.
-//  Copyright © 2018 Giovanni Gobbi. All rights reserved.
-//
-
 #include <stdio.h>
 #include "lib1718.h"
 
 int main(int argc, const char * argv[]) {
-    char * query = "INSERT INTO Scarpe (Marca,Taglia,Costo) VALUES (Nike,47,140)";
+    char * query = "INSERT QUERY HERE";
     bool ifOk = executeQuery(query);
     printf("%d ", ifOk);
     return 0;
